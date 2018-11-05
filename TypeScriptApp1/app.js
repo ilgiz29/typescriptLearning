6588a50657cbb230ead73eafd4a943c7b1b32faa
ilgiz <ilgizgilmanov29@gmail.com>
2018-11-05 12:56:18 +0300

    Начало

---
+++ TypeScriptApp1/app.js
@@ -0,0 +1,9 @@
+class User {
+    constructor(_name, _age) {
+        this.name = _name;
+        this.age = _age;
+    }
+}
+var tom = new User("Tom", 26);
+console.log("Name: " + tom.name + ", Age: " + tom.age);
+//# sourceMappingURL=app.js.map
---
+++ TypeScriptApp1/app.ts
@@ -0,0 +1,12 @@
+﻿class User {
+	name: string;
+	age: number;
+	constructor(_name: string, _age: number) {
+
+		this.name = _name;
+		this.age = _age;
+	}
+}
+
+var tom: User = new User("Tom", 26);
+console.log("Name: " + tom.name + ", Age: " + tom.age);
